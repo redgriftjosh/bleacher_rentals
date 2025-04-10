a0297299ea0b505cebadcfb09e8d079866d2965f
redgriftjosh <redgriftjosh@gmail.com>
2025-04-10 15:03:06 -0400

    GoTrueClient warning fix

--- src/utils/supabase/supabaseClient.ts
+++ src/utils/supabase/supabaseClient.ts
@@ -1,6 +1,21 @@
 import { createClient } from "@supabase/supabase-js";
+import { SupabaseClient } from "@supabase/supabase-js";
 
-export const supabaseClient = async (supabaseToken: string) => {
+const clients = new Map<string, SupabaseClient>();
+
+/* This stops the warning:
+GoTrueClient.ts:198 Multiple GoTrueClient instances detected in the same browser context. 
+It is not an error, but this should be avoided as it may produce undefined behavior when 
+used concurrently under the same storage key.
+
+Only creates client with the same token once.
+*/
+
+export const supabaseClient = (supabaseToken: string) => {
+  if (clients.has(supabaseToken)) {
+    return clients.get(supabaseToken)!;
+  }
+
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -12,5 +27,7 @@ export const supabaseClient = async (supabaseToken: string) => {
       },
     }
   );
+
+  clients.set(supabaseToken, supabase);
   return supabase;
 };
