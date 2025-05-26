11b81b2b66457a5afb03a1ad494ca70e5bfba4c6
redgriftjosh <redgriftjosh@gmail.com>
2025-05-26 14:27:35 -0400

    commenitng out realtime subscriptions

--- src/hooks/useSupabaseSubscriptions.ts
+++ src/hooks/useSupabaseSubscriptions.ts
@@ -17,112 +17,112 @@ import { useUserHomeBasesStore } from "@/state/userHomeBasesStore";
 // https://www.loom.com/share/50e15eaa6e0e4f8e9e063ab896ecd8a1?sid=41289fe0-9b87-4026-a6a4-8ef9bd14f331
 export default function useSupabaseSubscriptions() {
   const { getToken } = useAuth();
-  const [subscriptionId, setSubscriptionId] = useState(0); // increments ever 30 seconds to re-trigger subscription hooks
+  // const [subscriptionId, setSubscriptionId] = useState(0); // increments ever 30 seconds to re-trigger subscription hooks
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setSubscriptionId((id) => id + 1); // triggers unmount/remount of the hook
-    }, 30000);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setSubscriptionId((id) => id + 1); // triggers unmount/remount of the hook
+  //   }, 30000);
 
-    return () => clearInterval(interval);
-  }, []);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   const bleacherStore = useBleachersStore();
   useSetupTable({
     tableName: "Bleachers",
-    channelName: "bleachers-channel",
+    // channelName: "bleachers-channel",
     getToken,
     setStore: bleacherStore.setBleachers,
     stale: bleacherStore.stale,
     setStale: bleacherStore.setStale,
-    subscriptionId,
+    // subscriptionId,
   });
 
   const usersStore = useUsersStore();
   useSetupTable({
     tableName: "Users",
-    channelName: "users-channel",
+    // channelName: "users-channel",
     getToken,
     setStore: usersStore.setUsers,
     stale: usersStore.stale,
     setStale: usersStore.setStale,
-    subscriptionId,
+    // subscriptionId,
   });
 
   const homeBaseStore = useHomeBasesStore();
   useSetupTable({
     tableName: "HomeBases",
-    channelName: "homebases-channel",
+    // channelName: "homebases-channel",
     getToken,
     setStore: homeBaseStore.setHomeBases,
     stale: homeBaseStore.stale,
     setStale: homeBaseStore.setStale,
-    subscriptionId,
+    // subscriptionId,
   });
 
   const UserStatusesStore = useUserStatusesStore();
   useSetupTable({
     tableName: "UserStatuses",
-    channelName: "userstatuses-channel",
+    // channelName: "userstatuses-channel",
     getToken,
     setStore: UserStatusesStore.setUserStatuses,
     stale: UserStatusesStore.stale,
     setStale: UserStatusesStore.setStale,
-    subscriptionId,
+    // subscriptionId,
   });
 
   const UserHomeBasesStore = useUserHomeBasesStore();
   useSetupTable({
     tableName: "UserHomeBases",
-    channelName: "userhomebases-channel",
+    // channelName: "userhomebases-channel",
     getToken,
     setStore: UserHomeBasesStore.setUserHomeBases,
     stale: UserHomeBasesStore.stale,
     setStale: UserHomeBasesStore.setStale,
-    subscriptionId,
+    // subscriptionId,
   });
 
   const UserRolesStore = useUserRolesStore();
   useSetupTable({
     tableName: "UserRoles",
-    channelName: "userroles-channel",
+    // channelName: "userroles-channel",
     getToken,
     setStore: UserRolesStore.setUserRoles,
     stale: UserRolesStore.stale,
     setStale: UserRolesStore.setStale,
-    subscriptionId,
+    // subscriptionId,
   });
 
   const eventsStore = useEventsStore();
   useSetupTable({
     tableName: "Events",
-    channelName: "events-channel",
+    // channelName: "events-channel",
     getToken,
     setStore: eventsStore.setEvents,
     stale: eventsStore.stale,
     setStale: eventsStore.setStale,
-    subscriptionId,
+    // subscriptionId,
   });
 
   const addressesStore = useAddressesStore();
   useSetupTable({
     tableName: "Addresses",
-    channelName: "addresses-channel",
+    // channelName: "addresses-channel",
     getToken,
     setStore: addressesStore.setAddresses,
     stale: addressesStore.stale,
     setStale: addressesStore.setStale,
-    subscriptionId,
+    // subscriptionId,
   });
 
   const bleacherEventsStore = useBleacherEventsStore();
   useSetupTable({
     tableName: "BleacherEvents",
-    channelName: "bleacherevents-channel",
+    // channelName: "bleacherevents-channel",
     getToken,
     setStore: bleacherEventsStore.setBleacherEvents,
     stale: bleacherEventsStore.stale,
     setStale: bleacherEventsStore.setStale,
-    subscriptionId,
+    // subscriptionId,
   });
 }
