d2bf9dc7785f5139c4d677cfc061de722c2a85b1
redgriftjosh <redgriftjosh@gmail.com>
2025-05-27 14:05:10 -0400

    attempt fix pusher on vercel

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -2,10 +2,10 @@ import PusherServer from "pusher";
 import PusherClient from "pusher-js";
 
 export const pusherServer = new PusherServer({
-  appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID!,
-  key: process.env.NEXT_PUBLIC_PUSHER_PUBLISHABLE_KEY!,
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.PUSHER_PUBLISHABLE_KEY!,
   secret: process.env.PUSHER_SECRET_KEY!,
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+  cluster: process.env.PUSHER_CLUSTER!,
   useTLS: true,
 });
 
