346dd4b9b669252b396ad8cc417ba1a00c238808
redgriftjosh <redgriftjosh@gmail.com>
2025-09-05 19:41:52 -0400

    created event blocks with pinned labels

---
+++ src/features/dashboard/ui/EventSpan.ts
@@ -0,0 +1,89 @@
+import { Container, Graphics, Text } from "pixi.js";
+import { BleacherEvent } from "../db/client/bleachers";
+import { CELL_HEIGHT, CELL_WIDTH } from "../values/constants";
+
+export type EventSpanType = { start: number; end: number; ev: BleacherEvent };
+
+export class EventSpan extends Container {
+  private g: Graphics;
+  private eventLabel: Text;
+  private currentSpan?: EventSpanType;
+
+  constructor() {
+    super();
+    this.g = new Graphics();
+    this.addChild(this.g);
+
+    this.eventLabel = new Text({
+      text: "",
+      style: {
+        fontSize: 12,
+        fontWeight: "400",
+        fill: 0xffffff, // white text
+        // wordWrap: true,
+        // wordWrapWidth: CELL_WIDTH,
+        align: "left",
+      },
+    });
+    this.addChild(this.eventLabel);
+
+    this.visible = false;
+  }
+
+  draw(
+    span: EventSpanType,
+    visibleStartColumn: number,
+    visibleEndColumn: number,
+    rowY: number,
+    wrappedX: number
+  ) {
+    this.currentSpan = span;
+
+    const drawStart = Math.max(span.start, visibleStartColumn);
+    const drawEnd = Math.min(span.end, visibleEndColumn);
+    if (drawEnd < drawStart) {
+      this.hide();
+      return;
+    }
+
+    const x = (drawStart - visibleStartColumn) * CELL_WIDTH;
+    const y = rowY;
+    const width = (drawEnd - drawStart + 1) * CELL_WIDTH - 1;
+    const height = CELL_HEIGHT - 1;
+
+    this.g.clear();
+    this.g.rect(x, y, width, height).fill(0x3277a8);
+
+    if (this.needsPin(visibleStartColumn, wrappedX)) {
+      this.updatePinnedLabel(0, wrappedX);
+    } else {
+      // Update label text & position
+
+      this.eventLabel.x = x;
+      this.eventLabel.y = y;
+    }
+    this.eventLabel.text = span.ev.eventName;
+
+    this.visible = true;
+  }
+
+  updatePinnedLabel(labelOffsetPx: number, wrappedX: number) {
+    if (!this.visible) return;
+    this.eventLabel.x = labelOffsetPx + wrappedX;
+    // this.eventLabel.y = this.lastRect.y;
+  }
+
+  /** True if the span's real beginning is off the left edge of the viewport. */
+  needsPin(visStartCol: number, wrappedX: number): boolean {
+    if (!this.currentSpan) return false;
+    const s = this.currentSpan.start;
+    return s < visStartCol || (s === visStartCol && wrappedX > 0);
+  }
+
+  hide() {
+    this.g.clear();
+    this.eventLabel.text = "";
+    this.currentSpan = undefined;
+    this.visible = false;
+  }
+}
