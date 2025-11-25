6d0b29ee2e658ed5eaecc8861885d9adbcd5cb70
redgriftjosh <redgriftjosh@gmail.com>
2025-11-25 12:06:17 -0500

    test component for clerk auth & RLS

---
+++ src/utils/supabase/useClerkSupabaseClient.ts
@@ -0,0 +1,27 @@
+"use client";
+
+import { useMemo } from "react";
+import { useSession } from "@clerk/nextjs";
+import { createClient } from "@supabase/supabase-js";
+
+export function useClerkSupabaseClient() {
+  const { session } = useSession();
+
+  const client = useMemo(
+    () =>
+      createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, // or _KEY, just be consistent
+        {
+          async accessToken() {
+            // If you’re using the Supabase third-party auth integration,
+            // this can just be the Clerk session token.
+            return (await session?.getToken()) ?? null;
+          },
+        }
+      ),
+    [session]
+  );
+
+  return client;
+}
